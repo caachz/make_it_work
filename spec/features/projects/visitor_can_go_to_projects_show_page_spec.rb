4f7b9d4d5358f4402d295169f25892194ee70e8b
Cassie Achzenick <caachzenick@gmail.com>
2020-02-07 10:53:29 -0700

    Add user story 1 all tests passing

--- spec/features/projects/visitor_can_go_to_projects_show_page_spec.rb
+++ spec/features/projects/visitor_can_go_to_projects_show_page_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe "visitor sees projects name, material and the theme of the challe
   it "has a show page" do
     challenge1 = Challenge.create!(theme: "Roses", project_budget: "5")
     project1 = Project.create!(name: "Ruffles", material: "Bows", challenge: challenge1)
+    project2 = Project.create!(name: "flowers", material: "only real flowers", challenge: challenge1)
 
     visit "/projects"
 
     expect(page).to have_content("name: Ruffles")
     expect(page).to have_content("material: Bows")
+    expect(page).to have_content("challenge theme: Roses")
+
+    expect(page).to have_content("name: flowers")
+    expect(page).to have_content("material: only real flowers")
     expect(page).to have_content("challenge theme: Roses")
   end
 end
